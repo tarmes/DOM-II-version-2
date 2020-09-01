c98f8ff83afa54dc38fb6f6535871761608e34cc
tarmes <traarmes@gmail.com>
2020-09-01 16:49:51 -0400

    4/10

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Your code goes here
+
+// Click
+
+const logo = document.querySelector('.logo-heading');
+
+logo.addEventListener('click', function (event) {
+    event.target.style.color = 'green';
+})
+
+// Mouseover && Mouseout
+
+const allLinks = document.querySelectorAll('.main-navigation a');
+console.log(allLinks);
+
+allLinks.forEach(link => {
+    link.addEventListener('mouseover', evt => {
+        evt.target.style.fontSize = '3em'
+    })
+    link.addEventListener('mouseout', evt => {
+        evt.target.style.fontSize = '2em'
+    })
+})
+
+// Keydown
+
+const introBomb = document.querySelector('.intro p')
+
+document.addEventListener('keydown', evt => {
+    if (event.key === 'b') {
+        introBomb.textContent = 'BOOOOOOOOOOOOOOOOOOOOOOOOOOOM!!!!!!!!!!!!!!';
+        introBomb.style.fontSize = '10em';
+        introBomb.style.color = 'red';
+    }
+})
+
+
+
